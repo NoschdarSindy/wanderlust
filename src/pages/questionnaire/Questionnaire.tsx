613bf7d752407155fa62a0e269d3e1f8519a5c4a
NoschdarSindy <noschdar.sindy@gmail.com>
2025-05-18 15:38:47 +0200

    chk

--- src/pages/questionnaire/Questionnaire.tsx
+++ src/pages/questionnaire/Questionnaire.tsx
@@ -1,65 +1,79 @@
-import {
-  Box,
-  Slider,
-  Typography,
-  Button,
-  CircularProgress,
-} from "@mui/material";
-import React, { useCallback, useState } from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
 import "./questionnaire.css";
 import { EventName, storeJson } from "src/lib/client";
 import { Entry } from "../../../start-service.ts";
 import { getSite } from "src/lib/composables.ts";
-import LoadingOverlay from "src/components/LoadingOverlay.tsx";
-import { useCustomNavigate } from "src/components/NavigationProvider.tsx";
 
-const sliderConfig = {
-  min: 1,
-  max: 7,
-  defaultValue: 4,
-  step: 0.01,
-  minRateDescription: "Strongly Disagree",
-  maxRateDescription: "Strongly Agree",
+const defaultOpts = {
+  type: "radiogroup",
+  isRequired: true,
+  choices: [
+    { value: true, text: "Yes" },
+    { value: false, text: "No" },
+  ],
 };
 
-const stimuli: { value: string; label: string; intent: string }[] = [
+const stimuli: {
+  value: EventName;
+  label: (site?: string) => string;
+  intent: string;
+}[] = [
   {
     value: "cookies",
-    label: "the cookie banner",
+    label: (site) =>
+      `the cookie banner${site ? ` on the ${site} website` : ""}`,
     intent: "accept cookies",
   },
   {
     value: "geolocation",
-    label: "the website’s request for my location",
+    label: (site) =>
+      `the${site ? ` ${site}` : ""} website’s request for my location`,
     intent: "allow it",
   },
   {
     value: "notification",
-    label: "the website’s request to send me notifications",
+    label: (site) =>
+      `the${site ? ` ${site}` : ""} website’s request to send me notifications`,
     intent: "allow them",
   },
   {
     value: "travelProtection",
-    label: "the travel protection options",
+    label: (site) =>
+      `the travel protection options${site ? ` on the ${site} website` : ""}`,
     intent: "include them in my booking",
   },
   {
     value: "newsletter",
-    label: "the newsletter sign-up",
+    label: (site) =>
+      `the newsletter sign-up${site ? ` on the ${site} website` : ""}`,
     intent: "subscribe",
   },
 ];
 
-const dpDescriptions = {
-  interfaceInterference:
-    "Some websites design their interfaces so that certain options are harder to see or choose.",
-  forcedAction:
-    "Some websites make users do things like signing up or granting access even when it's not really necessary.",
-  socialEngineering:
-    "Some websites use emotional or guilt-driven wording to pressure users into agreeing to something.",
-  sneaking:
-    "Some websites add extra services or costs by default unless the user removes them.",
-};
+const dpDescriptions = [
+  {
+    value: "forcedAction",
+    description:
+      "Some websites make users do things like signing up or granting access even when it's not really necessary.",
+  },
+  {
+    value: "interfaceInterference",
+    description:
+      "Some websites design their interfaces so that certain options are harder to see or choose.",
+  },
+  {
+    value: "socialEngineering",
+    description:
+      "Some websites use emotional or guilt-driven wording to pressure users into agreeing to something.",
+  },
+  {
+    value: "sneaking",
+    description:
+      "Some websites add extra services or costs by default unless the user removes them.",
+  },
+];
 
 const initialQuestions = [
   {
@@ -67,286 +81,117 @@ const initialQuestions = [
     text: "I am familiar with deceptive designs (sometimes called ‘dark patterns’).",
   },
   {
-    value: "ddp_bother",
-    text: "Deceptive design techniques usually bother me when I encounter them online.",
+    value: "ddp_bothered",
+    text: "Deceptive design techniques generally bother me when I encounter them online.",
   },
 ];
 
+const generateSurveyModel = (sites) => {
+  return {
+    completedHtml:
+      "<h6>The study is now finished. Thank you for your participation.</h6> ",
+    pages: [
+      {
+        title:
+          "Thank you for your participation so far. Below, we'd like to ask some questions about your familiarity and experience with deceptive designs.",
+        elements: [
+          {
+            type: "panel",
+            elements: [
+              ...initialQuestions.map((q) => ({
+                name: q.value,
+                title: q.text,
+                ...defaultOpts,
+              })),
+            ],
+          },
+          ...dpDescriptions.map(({ value, description }) => ({
+            type: "panel",
+            elements: [
+              {
+                type: "html",
+                html: `<big>${description}</big>`,
+              },
+              {
+                name: `${value}_familiar`,
+                title: "I had seen this kind of design before the study.",
+                ...defaultOpts,
+              },
+              {
+                name: `${value}_bothered`,
+                title: "This kind of design generally bothers me.",
+                ...defaultOpts,
+              },
+            ],
+          })),
+        ],
+      },
+      ...sites.map((site, i) => {
+        const prefix = `${site.site.name}_${site.design}_`;
+        return {
+          elements: [
+            {
+              type: "html",
+              html: `<h6>Next, we’d like to ask you a few short questions about your experience with the <b>${site.site.item_name}</b> website (${site.domain}). Once you’ve completed this part, ${
+                i < sites.length - 1
+                  ? `you will be asked the same questions about the ${sites[i + 1].site.item_name} website.`
+                  : "the study will be finished."
+              }</h6>`,
+            },
+            ...stimuli.flatMap(({ value, label, intent }) => ({
+              type: "panel",
+              elements: [
+                {
+                  name: `${prefix + value}_aware`,
+                  title: `It seemed that ${label(site.site.item_name)} ${value === "travelProtection" ? "were" : "was"} designed to restrict my control or pressure me to ${intent}.`,
+                  ...defaultOpts,
+                },
+                {
+                  name: `${prefix + value}_bothered`,
+                  title: `The design of ${label()} bothered me.`,
+                  ...defaultOpts,
+                },
+              ],
+            })),
+          ],
+        };
+      }),
+    ],
+  };
+};
+
 export default function Questionnaire() {
-  const entries = JSON.parse(VITE_ENTRIES);
+  const entries = JSON.parse(import.meta.env.VITE_ENTRIES);
+  const sites = entries.map((site: Entry) => ({
+    site: getSite(site.site),
+    domain: site.domain,
+    design: site.design,
+  }));
 
-  const sites = entries.map((site: Entry) => {
-    return {
-      site: getSite(site.site),
-      domain: site.domain,
-      design: site.design,
-    };
-  });
+  const survey = new Model(generateSurveyModel(sites));
 
-  const delay = (func: () => void) => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      func();
-    }, 400);
+  const meta = {
+    darkFirst: sites[0].design === "dark",
+    hotelFirst: sites[0].site.name === "hotels",
   };
 
-  const [currentSiteIndex, setCurrentSiteIndex] = useState(0);
-  const [showInitialQuestion, setShowInitialQuestion] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-  const currentSite = sites[currentSiteIndex];
-
-  const prefix = `${currentSite.site.name}_${currentSite.site.design}_`;
-  const siteQuestions = stimuli.flatMap(({ value, label, intent }) => [
-    {
-      value: `${prefix + value}_aware`,
-      text: `It seemed that the design of ${label} on the ${currentSite} website was intended to restrict my control or pressure me to ${intent}.`,
-    },
-    {
-      value: `${prefix + value}_bothered`,
-      text: `I was bothered by the design of ${label} on the ${currentSite} website.`,
-    },
-  ]);
-
-  const [responses, setResponses] = useState<
-    Record<string, Record<string, number>>
-  >({
-    initial: { [initialQuestions.value]: sliderConfig.defaultValue },
-    ...sites.reduce(
-      (acc, site) => ({
-        ...acc,
-        [site.site.item_name]: siteQuestions.reduce(
-          (qAcc, q) => ({ ...qAcc, [q.value]: sliderConfig.defaultValue }),
-          {},
-        ),
-      }),
-      {},
-    ),
-  });
-
-  const [touchedSliders, setTouchedSliders] = useState<
-    Record<string, Record<string, boolean>>
-  >({
-    initial: { [initialQuestions.value]: false },
-    ...sites.reduce(
-      (acc, site) => ({
-        ...acc,
-        [site.site.item_name]: siteQuestions.reduce(
-          (qAcc, q) => ({ ...qAcc, [q.value]: false }),
-          {},
-        ),
-      }),
-      {},
-    ),
-  });
-
-  const [error, setError] = useState<string | null>(null);
-
-  const handleSliderChange = (
-    _: any,
-    value: number | number[],
-    name: string,
-    section: string,
-  ) => {
-    setResponses((prev) => ({
-      ...prev,
-      [section]: {
-        ...prev[section],
-        [name]: value as number,
-      },
-    }));
-    setTouchedSliders((prev) => ({
-      ...prev,
-      [section]: {
-        ...prev[section],
-        [name]: true,
-      },
-    }));
-    setError(null);
-  };
-
-  const handleSliderClick = (name: string, section: string) => {
-    setTouchedSliders((prev) => ({
-      ...prev,
-      [section]: {
-        ...prev[section],
-        [name]: true,
-      },
-    }));
-    setError(null);
-  };
-
-  const handleNext = () => {
-    if (showInitialQuestion) {
-      if (!touchedSliders.initial[initialQuestions.value]) {
-        setError("Please interact with the slider before proceeding.");
-        return;
-      }
-      delay(() => {
-        setShowInitialQuestion(false);
-        setError(null);
-      });
-      return;
-    }
-
-    const allTouched = siteQuestions.every(
-      (q) => touchedSliders[currentSite.site.item_name][q.value],
-    );
-
-    if (!allTouched) {
-      setError("Please interact with all sliders before proceeding.");
-      return;
-    }
-
-    if (currentSiteIndex < sites.length - 1) {
-      delay(() => {
-        setCurrentSiteIndex(currentSiteIndex + 1);
-        setError(null);
-      });
-    } else {
-      const results = JSON.stringify({ responses, entries });
-      console.log(results);
-
-      storeJson(results, "questionnaire")
-        .then((response) => {
-          response.json().then((json) => {
-            console.log(json.message);
+  survey.onComplete.add((sender) => {
+    Object.entries({ questionnaire: sender.data, meta }).forEach(
+      ([key, value]) => {
+        const result = JSON.stringify(value);
+        console.log(result);
+        storeJson(result, key)
+          .then((response) => {
+            response.json().then((json) => {
+              console.log(json.message);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
           });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      alert("Thank you for your participation. The study is now finished.");
-    }
-  };
+      },
+    );
+  });
 
-  return (
-    <>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-        padding={3}
-      >
-        <h3>Questionnaire</h3>
-        <br />
-        {showInitialQuestion ? (
-          <>
-            <h5>
-              Thank you for your participation so far. Below, please answer the
-              following question about your familiarity with deceptive designs.
-            </h5>
-            <br />
-            <br />
-            <Box sx={{ mb: 4, width: "80%" }}>
-              <Typography variant="body1" gutterBottom>
-                {initialQuestions.text}
-              </Typography>
-              <Slider
-                value={
-                  responses.initial[initialQuestions.value] ||
-                  sliderConfig.defaultValue
-                }
-                onChange={(e, value) =>
-                  handleSliderChange(
-                    e,
-                    value,
-                    initialQuestions.value,
-                    "initial",
-                  )
-                }
-                onClick={() =>
-                  handleSliderClick(initialQuestions.value, "initial")
-                }
-                min={sliderConfig.min}
-                max={sliderConfig.max}
-                step={sliderConfig.step}
-                valueLabelDisplay="off"
-                aria-labelledby={`${initialQuestions.value}-slider`}
-              />
-              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                <Typography variant="body2">
-                  {sliderConfig.minRateDescription}
-                </Typography>
-                <Typography variant="body2">
-                  {sliderConfig.maxRateDescription}
-                </Typography>
-              </Box>
-            </Box>
-          </>
-        ) : (
-          <>
-            <h5>
-              Next, we'd like to ask you a few brief questions about your
-              experience with the <b>{currentSite.site.item_name} website</b> (
-              {currentSite.domain}). After completing this questionnaire,{" "}
-              {currentSiteIndex < sites.length - 1
-                ? `you will answer the same questions for the ${sites[currentSiteIndex + 1].site.item_name} website.`
-                : "the study will be finished."}
-            </h5>
-            <br />
-            <br />
-            {siteQuestions.map((q) => (
-              <Box key={q.value} sx={{ mb: 4, width: "80%" }}>
-                <Typography variant="body1" gutterBottom>
-                  {q.text}
-                </Typography>
-                <Slider
-                  value={
-                    responses[currentSite.site.item_name][q.value] ||
-                    sliderConfig.defaultValue
-                  }
-                  onChange={(e, value) =>
-                    handleSliderChange(
-                      e,
-                      value,
-                      q.value,
-                      currentSite.site.item_name,
-                    )
-                  }
-                  onClick={() =>
-                    handleSliderClick(q.value, currentSite.site.item_name)
-                  }
-                  min={sliderConfig.min}
-                  max={sliderConfig.max}
-                  step={sliderConfig.step}
-                  valueLabelDisplay="off"
-                  aria-labelledby={`${q.value}-slider`}
-                />
-                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                  <Typography variant="body2">
-                    {sliderConfig.minRateDescription}
-                  </Typography>
-                  <Typography variant="body2">
-                    {sliderConfig.maxRateDescription}
-                  </Typography>
-                </Box>
-              </Box>
-            ))}
-          </>
-        )}
-        {error && (
-          <Typography color="error" sx={{ mt: 2 }}>
-            {error}
-          </Typography>
-        )}
-        <Box sx={{ position: "relative", mt: 2 }}>
-          <Button
-            onClick={handleNext}
-            variant="contained"
-            sx={{ textTransform: "none" }}
-            disabled={isLoading}
-          >
-            {showInitialQuestion
-              ? "Next"
-              : currentSiteIndex < sites.length - 1
-                ? "Next"
-                : "Complete"}
-          </Button>
-          {isLoading && <LoadingOverlay />}
-        </Box>
-      </Box>
-    </>
-  );
+  return <Survey model={survey} />;
 }
